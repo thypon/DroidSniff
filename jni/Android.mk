aa15f510a0d789e722075cf986e84629103552c0
Andrea Brancaleoni <miwaxe@gmail.com>
2014-01-18 16:00:18 +0100

    Working JNI config

--- jni/Android.mk
+++ jni/Android.mk
@@ -1,1 +1,16 @@
-include $(call all-subdir-makefiles)
+MY_OLD_PATH := $(call my-dir)
+LOCAL_PATH := $(MY_OLD_PATH)
+
+include $(LOCAL_PATH)/libnet/Android.mk
+
+LOCAL_PATH := $(MY_OLD_PATH)
+
+include $(LOCAL_PATH)/libpcap/Android.mk
+
+LOCAL_PATH := $(MY_OLD_PATH)
+
+include $(LOCAL_PATH)/droidsniff/Android.mk
+
+LOCAL_PATH := $(MY_OLD_PATH)
+
+include $(LOCAL_PATH)/arpspoof/Android.mk
--- jni/arpspoof/Android.mk
+++ jni/arpspoof/Android.mk
@@ -6,9 +6,9 @@ LOCAL_MODULE := arpspoof
 
 LOCAL_SRC_FILES := arpspoof/arp.c  \
                    arpspoof/arpspoof.c \
-		   arpspoof/ensure_death.c
+		           arpspoof/ensure_death.c
                    
 APP_OPTIM := release
-LOCAL_C_INCLUDES := libpcap libnet/include include
+LOCAL_C_INCLUDES := libpcap libnet/include arpspoof/include include
 LOCAL_STATIC_LIBRARIES := libpcap libnet
 include $(BUILD_EXECUTABLE)
