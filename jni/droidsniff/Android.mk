8c3115bc8df68bde0ebb2007d77774e6a2a0713f
Andrea Brancaleoni <miwaxe@gmail.com>
2014-01-18 15:08:16 +0100

    Joined Common Static Libraries

--- jni/droidsniff/Android.mk
+++ jni/droidsniff/Android.mk
@@ -11,5 +11,3 @@ APP_OPTIM := release
 LOCAL_C_INCLUDES := libpcap libnet/include include
 LOCAL_STATIC_LIBRARIES := libpcap libnet
 include $(BUILD_EXECUTABLE)
-include $(MY_LOCAL_PATH)/libpcap/Android.mk
-include $(MY_LOCAL_PATH)/libnet/Android.mk
